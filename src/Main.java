eca115d944eaa7032cbb3f3cec0d65a4ea0517dc
mochkar.ne <mochkar.ne@esphere.ru>
2025-08-22 22:27:17 +0700

    Update main function to calculate the basic operation of two input numbers

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма равна: " + sum);
+
+        int diff = firstNumber - secondNumber;
+        System.out.println("Разность равна: " + diff);
+
+        int multiplication = firstNumber * secondNumber;
+        System.out.println("Произведение равно: " + multiplication);
+
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное равно: " + quotient);
     }
 }

db04452864ee727129c3a05764a7970886d83346
mochkar.ne <mochkar.ne@esphere.ru>
2025-09-05 18:59:07 +0700

    Homework on the topic of cycles

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int i = 1;
+       while (true) {
+           System.out.println("Введите путь к файлу");
+           String path = new Scanner(System.in).nextLine();
+           File file = new File(path);
+           boolean fileExists = file.exists();
+           boolean isDirectory = file.isDirectory();
+           if(!fileExists) {
+               System.out.println("Введен путь к несуществующему файлу!Попробуйте снова.");
+               continue;}
+           if(isDirectory) {
+               System.out.println("Введен путь к папке, а не файлу!Попробуйте снова.");
+               continue;}
+           System.out.println("Путь указан верно.");
+           System.out.println("Это файл номер " + i + "");
+           i++;
+
+       }
     }
 }
